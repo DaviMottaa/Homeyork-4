b8699316441d7728a1c46506130caa52885c2861
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-06-18 18:12:19 -0300

    Add files via upload

---
+++ Davi_18621.py
@@ -0,0 +1,47 @@
+#Question2
+Lista = ["Maçã", "Laranja","Banana","Tangerina","Melancia"]
+
+#Question3
+print(Lista)
+print(Lista[1])
+
+#Question4
+Lista.sort()
+
+#Question5
+print(Lista)
+
+#Question6
+Lista.append("apple")
+
+#Question7
+print(Lista)
+
+#Question8
+Lista.insert(0, "zucchini")
+
+#Question9
+print(Lista)
+
+#Question10
+
+
+#Question11
+
+#Question12
+Lista.append("Melão")
+
+#Question13
+print(Lista)
+
+#Question14
+for item in Lista:
+    print(Lista)
+
+#Question15
+for item in Lista.upper:
+    print(Lista)
+
+#Question16
+for item in Lista:
+    print(Lista.upper().lower())
